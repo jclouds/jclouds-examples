c8d3f616070f4712e32f6446a3d273614d2db7df
Benjamin Atkin <ben@benatkin.com>
2012-02-05 00:09:27 -0700

    fix url

--- blobstore-clojure/project.clj
+++ blobstore-clojure/project.clj
@@ -1,6 +1,6 @@
 (defproject blobstore-clojure "1.0-SNAPSHOT"
   :description "create and list buckets"
-  :url "https://github.com/jclouds/jclouds-examples/tree/master/jclouds-examples/blobstore-clojure"
+  :url "https://github.com/jclouds/jclouds-examples/tree/master/blobstore-clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/core.incubator "0.1.0"]

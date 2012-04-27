724cb23cce119978b8b3217e3c127ca14f2e0c57
Andrea Turli <andrea.turli@gmail.com>
2012-04-27 13:31:58 +0200

    fixed reflections issue

--- play-compute/project/Build.scala
+++ play-compute/project/Build.scala
@@ -9,10 +9,12 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "com.google.guava" % "guava" % "12.0-rc2",
-      "org.jclouds" % "jclouds-compute" % "1.5.0-alpha.5"
+      "org.jclouds" % "jclouds-compute" % "1.5.0-alpha.5",
+      "org.reflections" % "reflections" % "0.9.7-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+      resolvers += "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository",
       resolvers += "Maven central" at "http://repo1.maven.org/maven2/",
       resolvers += "Sonatype snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
     )

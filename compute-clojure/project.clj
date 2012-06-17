ca78354cbcfe474f808b5a61effcdbc01bd827b1
Adrian Cole <adrian@jclouds.org>
2012-06-17 10:51:35 -0400

    updated examples to 1.5.0-beta.3

--- compute-clojure/project.clj
+++ compute-clojure/project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/core.incubator "0.1.0"]
-                 [org.jclouds/jclouds-allcompute "1.5.0-beta.1"]
-                 [org.jclouds/driver/jclouds-sshj "1.5.0-beta.1"]
-                 [org.jclouds/driver/jclouds-slf4j "1.5.0-beta.1"]
+                 [org.jclouds/jclouds-allcompute "1.5.0-beta.3"]
+                 [org.jclouds/driver/jclouds-sshj "1.5.0-beta.3"]
+                 [org.jclouds/driver/jclouds-slf4j "1.5.0-beta.3"]
                  [ch.qos.logback/logback-classic "1.0.0"]])
